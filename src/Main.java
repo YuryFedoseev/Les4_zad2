adb084a6510e588bccf243491f5312efd52994e2
Yury.Fedoseev <Yri95@inbox.ru>
2021-06-28 21:09:15 +0300

    Правки переменных

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService  service = new BmiService ();
-        double rost = 1.6;//рост тела в метрах
-        double massa = 77;//вес тела в кг
-        double index = service.calculate(massa , rost);
+        double height = 1.6;//рост тела в метрах
+        double weight = 77;//вес тела в кг
+        double index = service.calculate(weight , height);
         System.out.println(index);
 
     }
